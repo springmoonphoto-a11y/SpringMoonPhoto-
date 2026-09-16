2dada1ae30229c8608e031471adab63a4069a35d
springmoonphoto-a11y <springmoonphoto@gmail.com>
2026-09-16 19:17:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SpringMoonGallery Service Worker
 // Verziószámot növelni minden új deployment előtt, hogy a cache frissüljön
-const CACHE = "springmoon-v88";
+const CACHE = "springmoon-v89";
 const SHELL = ["/", "/index.html", "/manifest.json"];
 
 self.addEventListener("install", e => {
